317c196a7432da4712d42b8363a55b06e5bcad70
javajigi <javajigi@gmail.com>
2019-08-15 10:56:16 +0900

    refactor: add wrapper to WebTestClient

--- src/test/java/myblog/user/web/UserAcceptanceTest.java
+++ src/test/java/myblog/user/web/UserAcceptanceTest.java
@@ -3,20 +3,18 @@ package myblog.user.web;
 import myblog.user.domain.User;
 import myblog.user.dto.UserCreatedDto;
 import myblog.user.dto.UserUpdatedDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 import support.test.AcceptanceTest;
+import support.test.NsWebTestClient;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 public class UserAcceptanceTest extends AcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(UserAcceptanceTest.class);
@@ -24,59 +22,33 @@ public class UserAcceptanceTest extends AcceptanceTest {
     @Value("${local.server.port}")
     private int port;
 
+    private NsWebTestClient webClient;
+
+    @BeforeEach
+    void setUp() {
+        webClient = NsWebTestClient.of(port);
+    }
+
     @Test
     @DisplayName("사용자 회원가입/조회/수정/삭제")
     void crud() {
         // 회원가입
         UserCreatedDto expected = new UserCreatedDto("javajigi", "javajigi@nextstep.camp", "password");
-        EntityExchangeResult<byte[]> response = clientBuilder().build()
-                .post()
-                .uri("/users")
-                .body(Mono.just(expected), UserCreatedDto.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody()
-                .returnResult();
-        URI location = response.getResponseHeaders().getLocation();
+        URI location = webClient.createResource("/users", expected, UserCreatedDto.class);
         logger.debug("location : {}", location);
 
         // 조회
-        User actual = clientBuilder().build()
-                .get()
-                .uri(location.toString())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class)
-                .returnResult().getResponseBody();
+        User actual = webClient.getResource(location, User.class);
         assertThat(actual.getUserId()).isEqualTo(expected.getUserId());
         assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
 
         // 수정
         UserUpdatedDto updateUser = new UserUpdatedDto("sanjigi", "sanjigi@nextstep.camp");
-        clientBuilder()
-                .filter(basicAuthentication(expected.getUserId(), expected.getPassword()))
-                .build()
-                .put()
-                .uri(location.toString())
-                .body(Mono.just(updateUser), UserUpdatedDto.class)
-                .exchange()
-                .expectStatus().isOk();
+        webClient.basicAuth(expected.getUserId(), expected.getPassword())
+                .updateResource(location, updateUser, UserUpdatedDto.class);
 
-
-        actual = clientBuilder().build()
-                .get()
-                .uri(location.toString())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class)
-                .returnResult().getResponseBody();
+        actual = webClient.getResource(location, User.class);
         assertThat(actual.getUserId()).isEqualTo(updateUser.getUserId());
         assertThat(actual.getEmail()).isEqualTo(updateUser.getEmail());
-    }
-
-    private WebTestClient.Builder clientBuilder() {
-        return WebTestClient
-                .bindToServer()
-                .baseUrl("http://localhost:" + port);
     }
 }
