790574dbc765425027749f8345d57660b2802547
javajigi <javajigi@gmail.com>
2022-08-12 10:05:40 +0900

    fix(handlebars): fix quotes in helper

--- src/main/java/support/handlebars/BlogHandlebarsHelper.java
+++ src/main/java/support/handlebars/BlogHandlebarsHelper.java
@@ -1,6 +1,8 @@
 package support.handlebars;
 
+import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Options;
+import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,12 @@ public class BlogHandlebarsHelper {
     @Autowired
     private BlogVersion blogVersion;
 
-    public String staticUrls(String path, Options options) {
+    public CharSequence staticUrls(String path, Options options) {
+        if (Strings.isBlank(path)) {
+            return Strings.EMPTY;
+        }
+
         logger.debug("static url : {}", path);
-        return String.format("/resources/%s%s", blogVersion.getVersion(), path);
+        return new Handlebars.SafeString(String.format("/resources/%s%s", blogVersion.getVersion(), path));
     }
 }
